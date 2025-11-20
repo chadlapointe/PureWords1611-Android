7e97910207c6901af85e9a6c0ad1715fd61a1049
chadlapointe <chadlapointe@gmail.com>
2025-11-20 12:27:25 -0700

    Initial commit – Pure Words 1611 (Psalm 12:6–7 KJV app)

---
+++ app/src/main/java/com/chadlapointe/purewords1611/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chadlapointe.purewords1611
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.view.View
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
